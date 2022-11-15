f53192609415a9c93c2bcdabba43e7d32d73585f
donghwan <wkazxf@gmail.com>
2022-11-15 14:20:59 +0900

    [FEAT] Override Configuration For CORS Issue

---
+++ src/main/kotlin/com/se/scramble/config/WebMvcConfig.kt
@@ -0,0 +1,15 @@
+package com.se.scramble.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig() : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("PUT", "POST", "GET", "DELETE")
+    }
+}
